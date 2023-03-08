f94e941dfe864a1639e1697ede9e9289c4d7032d
Lucas Escoto da Luz <108088705+LucasEscotoDaLuz@users.noreply.github.com>
2023-03-08 18:29:27 -0300

    Sequencia de  Fibonacci

---
+++ SequenciaFibonacci/SequenciaFibonacci/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class SequenciaFibonacci
+{
+    static void Main(string[] args)
+    {
+        int elemento1 = 0;
+        int elemento2 = 1;
+        int auxiliar;
+
+        for (int i = 0; i < 20; i++)
+        {
+            auxiliar = elemento1;
+            elemento1 = elemento2;
+            elemento2 = elemento1 + auxiliar;
+            Console.WriteLine("{0}", elemento2);
+        }
+
+        Console.ReadLine();
+    }
+}

d5243ddfb4a4615ec75eb9e3507e7e8648359326
Lucas Escoto da Luz <108088705+LucasEscotoDaLuz@users.noreply.github.com>
2023-03-08 18:30:46 -0300

    Aplicação Inversão de String

---
+++ InverterStrings/Desafio/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+
+    public static string Reverse(string Input)
+    {
+        char[] stringArray = Input.ToCharArray();
+        string invertString = String.Empty;
+        
+        for (int i = stringArray.Length - 1; i > -1; i--)
+        {
+            invertString += stringArray[i];
+        }
+        return invertString;
+    }
+
+
+    static void Main(string[] args)
+    {
+        //Insira a string desejada
+        Console.WriteLine(Reverse("MeContratePorFavor"));
+        Console.WriteLine(Reverse("AprendoRapido"));
+        Console.WriteLine(Reverse("SouDedicado"));
+        Console.WriteLine(Reverse("VaiDarCerto"));
+    }
+}
